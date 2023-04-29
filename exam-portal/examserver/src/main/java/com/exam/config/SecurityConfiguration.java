304a6632817a07f709a320de821b99a1b38362c1
Adity Shaw <adityshaw99@gmail.com>
2023-04-29 13:04:36 +0530

    Revert "commit"
    This reverts commit 56fd70c3d8470db0e1d9df1903c49f7de82eca63.

--- exam-portal/examserver/src/main/java/com/exam/config/SecurityConfiguration.java
+++ exam-portal/examserver/src/main/java/com/exam/config/SecurityConfiguration.java
@@ -35,8 +35,8 @@ public class SecurityConfiguration {
     private Object jwtAuthenticationEntryPoint;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder  passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
